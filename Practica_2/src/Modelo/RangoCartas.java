f5fe15ce5dc3a89f2e369a9de9420b7916c210a3
Vuztulk <luisbalo2001@gmail.com>
2024-10-06 13:04:23 +0200

    a

--- Practica_2/src/Modelo/RangoCartas.java
+++ Practica_2/src/Modelo/RangoCartas.java
@@ -283,29 +283,22 @@ public class RangoCartas {
 
     private void agregarRango(StringBuilder resultado, String rangoInicio, String rangoFin, boolean incluyeAA) {
         if (incluyeAA) {
-            // Si el rango incluye AA, agregamos como RangoInicio+
             resultado.append(rangoFin).append("+,");
         } else if (rangoInicio.equals(rangoFin)) {
-            // Caso 1: Si el inicio es igual al fin, agregamos sin ningún símbolo adicional
             resultado.append(rangoInicio).append(",");
         } else if (esRangoConsecutivo(rangoInicio, rangoFin)) {
-            // Caso 2: Si es un rango consecutivo (por ejemplo, "KK-JJ"), agregamos con guion
             resultado.append(rangoFin).append("-").append(rangoInicio).append(",");
         } else {
-            // Caso 3: Si es un rango que puede extenderse hacia arriba (por ejemplo, "77+"), agregamos "+"
             resultado.append(rangoFin).append("+,");
         }
     }
 
     private boolean esRangoConsecutivo(String rangoInicio, String rangoFin) {
-        int indiceInicio = ordenCartas.indexOf(rangoInicio.charAt(0));
-        int indiceFin = ordenCartas.indexOf(rangoFin.charAt(0));
+        int indiceInicio = ordenCartas.indexOf(rangoInicio.charAt(1));
+        int indiceFin = ordenCartas.indexOf(rangoFin.charAt(1));
 
-        // Si el rangoInicio y rangoFin están en posiciones consecutivas
-        return indiceInicio - indiceFin == 1;
+        return (indiceFin - indiceInicio != ordenCartas.indexOf(rangoFin.charAt(0)) - 1);
     }
-
-
 
 	public Set<String> getRangos() {
 		return rangos;
